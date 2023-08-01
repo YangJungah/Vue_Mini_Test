af75e497cacbf85daba1d05b9248d94781d36fa5
YangJungah <wjddk1914@naver.com>
2023-08-01 15:05:24 +0900

    내용수정함

--- two/src/components/HelloWorld.vue
+++ two/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h5>{{ msg }}</h5>
   </div>
 </template>
 
@@ -13,7 +13,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
